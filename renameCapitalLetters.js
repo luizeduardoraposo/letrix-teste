ee3b329d53416f3316f2b9b0bae99e87a9c568e8
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2025-08-24 06:49:05 -0300

    add renameCapitalLetters.js to convert uppercase letters to lowercase in words-ptbr-anagramas.txt

---
+++ renameCapitalLetters.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const FILE_PATH = path.join(__dirname, 'words-ptbr-anagramas.txt');
+
+const content = fs.readFileSync(FILE_PATH, 'utf-8');
+const lowerContent = content.toLowerCase();
+fs.writeFileSync(FILE_PATH, lowerContent);
+console.log('Todas as letras maiúsculas foram convertidas para minúsculas em words-ptbr-anagramas.txt');
