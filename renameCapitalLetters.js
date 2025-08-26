eea06325b827b800a61c0bcb704c4a924ab5b90f
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2025-08-26 01:06:27 -0300

    fix: update file path and log message in renameCapitalLetters.js to reflect correct filename

--- renameCapitalLetters.js
+++ renameCapitalLetters.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const FILE_PATH = path.join(__dirname, 'words-ptbr-anagramas.txt');
+const FILE_PATH = path.join(__dirname, 'words-ptbr.txt');
 
 const content = fs.readFileSync(FILE_PATH, 'utf-8');
 const lowerContent = content.toLowerCase();
 fs.writeFileSync(FILE_PATH, lowerContent);
-console.log('Todas as letras maiúsculas foram convertidas para minúsculas em words-ptbr-anagramas.txt');
+console.log('Todas as letras maiúsculas foram convertidas para minúsculas em words-ptbr.txt');
