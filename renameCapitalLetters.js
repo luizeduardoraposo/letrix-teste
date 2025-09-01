02b8e174f929b41d9041573d296af18e04400b03
Luiz Eduardo Raposo <luizeduardoraposo@protonmail.com>
2025-08-31 21:41:59 -0300

    Implement code changes to enhance functionality and improve performance

--- renameCapitalLetters.js
+++ renameCapitalLetters.js
@@ -1,9 +1,10 @@
+
 const fs = require('fs');
 const path = require('path');
 
 const FILE_PATH = path.join(__dirname, 'words-ptbr.txt');
 
 const content = fs.readFileSync(FILE_PATH, 'utf-8');
-const lowerContent = content.toLowerCase();
-fs.writeFileSync(FILE_PATH, lowerContent);
-console.log('Todas as letras maiúsculas foram convertidas para minúsculas em words-ptbr.txt');
+const upperContent = content.toUpperCase();
+fs.writeFileSync(FILE_PATH, upperContent);
+console.log('Todas as letras foram convertidas para MAIÚSCULAS em words-ptbr.txt');
